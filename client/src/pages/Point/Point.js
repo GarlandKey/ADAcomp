e79559117ecee97c24dfc611dd0afd6bfbabb313
Garland Key <hello@garlandkey.com>
2023-01-27 16:29:08 -0500

    Decided that Tailwind isn't the best approach for this project. Renammed legacy css filed for re-integration

--- client/src/pages/Point/Point.js
+++ client/src/pages/Point/Point.js
@@ -1,22 +1,11 @@
 import TopNav from "../../components/TopNav/TopNav";
 import PointNav from "../../components/PointNav/PointNav";
 import PointForm from "../../components/PointForm/PointForm";
-// import "./Point.css";
+include "./Point.css";
 
 const Point = () => {
     return (
-        <div
-			className="
-				bg-darkest
-				flex
-				flex-row
-				flex-wrap
-				h-screen
-				gap-8
-				space-around
-				text-lightest
-				w-screen"
-		>
+        <div className="">
             <TopNav />
             <PointNav />
             <PointForm />
